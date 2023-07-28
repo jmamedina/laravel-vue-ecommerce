26b8cc62d24070256b94f9fa78c562a370067eea
jmamedina <johnmichaelmedina1@gmail.com>
2023-07-28 11:58:41 +0900

    update user request fix

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
 
 class UpdateUserRequest extends FormRequest
 {
@@ -24,7 +26,7 @@ class UpdateUserRequest extends FormRequest
         return [
             'name' => ['max:55'],
             'email' => ['email'],
-            'password' => ['nullable', Password::min(8)->numbers()->letters()->symbols()]
+            'password' => ['nullable', Password::min(8)]
         ];
     }
 }
