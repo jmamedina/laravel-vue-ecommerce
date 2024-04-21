f9b834ac9e4995471cb55c50809cef8275f03a87
jmamedina <johnmichaelmedina1@gmail.com>
2024-04-21 12:12:21 +0900

    add japan in countries

---
+++ database/seeders/JapanSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Country;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use PHPUnit\Framework\Constraint\Count;
+
+class JapanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $japanStates = [
+            "KYU" => 'Kushu',
+            "SHI" => 'Shikoku',
+            "KAN" => 'Kantou',
+            "HDO" => 'Hokkaid',
+            "KAN" => 'Kansai',
+            "CHU" => 'Chugoku',
+        ];
+
+        $countries = [
+            ['code' => 'jp', 'name' => 'Japan', 'states' => json_encode($japanStates)],
+        ];
+        Country::insert($countries);
+
+    }
+}
